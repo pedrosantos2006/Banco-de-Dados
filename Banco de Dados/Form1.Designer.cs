9d46e7b0d515b657f650ecd3e817686de85b52d7
Pedro Santos <112526196+pedrosantos2006@users.noreply.github.com>
2023-04-20 20:55:45 -0300

    Adicionar arquivos de projeto.

---
+++ Banco de Dados/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Banco_de_Dados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextBoxPesquisa = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ButtonCadastro = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Heavy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(63, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pesquisa";
+            // 
+            // TextBoxPesquisa
+            // 
+            this.TextBoxPesquisa.Location = new System.Drawing.Point(158, 50);
+            this.TextBoxPesquisa.Name = "TextBoxPesquisa";
+            this.TextBoxPesquisa.Size = new System.Drawing.Size(389, 20);
+            this.TextBoxPesquisa.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(67, 99);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(660, 241);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // ButtonCadastro
+            // 
+            this.ButtonCadastro.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ButtonCadastro.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.ButtonCadastro.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ButtonCadastro.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.ButtonCadastro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonCadastro.Location = new System.Drawing.Point(629, 378);
+            this.ButtonCadastro.Name = "ButtonCadastro";
+            this.ButtonCadastro.Size = new System.Drawing.Size(134, 42);
+            this.ButtonCadastro.TabIndex = 3;
+            this.ButtonCadastro.Text = "Cadastro de Novos Dados";
+            this.ButtonCadastro.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Banco_de_Dados.Properties.Resources.istockphoto_1188511715_612x612;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ButtonCadastro);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.TextBoxPesquisa);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextBoxPesquisa;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button ButtonCadastro;
+    }
+}
+
---
+++ Banco de Dados/Form1.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banco_de_Dados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection("server=localhost;user=root;database=pokeagenda;password=;");
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM t_pokemon", conn);
+
+            // DataTable = armazenar os resultados da consulta
+            DataTable dt = new DataTable();
+            conn.Open();
+
+            // MySqlDataAdapter = preencher os resultados da consulta
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            conn.Close();
+
+            //Vincule o DataTable ao DataGridView para exibir os dados na grade
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
